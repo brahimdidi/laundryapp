e5c390959faa0f9a6c7e2ac0bb5686e53042aa7d
brahim didi <brahimgb1team@gmail.com>
2023-08-07 18:25:28 +0000

    create cart and pickup screen with logic date and time of pickup

---
+++ screens/CartScreen.js
@@ -0,0 +1,317 @@
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useDispatch, useSelector } from "react-redux";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import {
+  addToCart,
+  decrementQuantity,
+  removeFromCart,
+} from "../redux/CartReducer";
+import {
+  decrementProductQuantity,
+  incrementProductQuantity,
+  resetProductQuantity,
+} from "../redux/ProductReducer";
+
+const CartScreen = () => {
+  const route = useRoute();
+  const cart = useSelector((state) => state.cart.cart);
+  const total = cart
+    .map((item) => item.price * item.quantity)
+    .reduce((a, b) => a + b, 0);
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <SafeAreaView>
+        <View style={{ margin: 10 }}>
+          {/* render back button */}
+          <Ionicons
+            onPress={() => navigation.goBack()}
+            name="arrow-back"
+            size={24}
+            color="black"
+          />
+        </View>
+        {total === 0 ? (
+          <View
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100%",
+            }}
+          >
+            <Text style={{ fontSize: 20, fontWeight: "bold", color: "red" }}>
+              Your cart is empty
+            </Text>
+          </View>
+        ) : (
+          <>
+            <Text style={{ textAlign: "center", fontSize: 20 }}>
+              Your Bucket
+            </Text>
+            {/* list of cart items with option to add or remove quantity */}
+            <ScrollView
+              style={{
+                maxHeight: 300,
+                borderRadius: 4,
+                backgroundColor: "white",
+                marginHorizontal: 10,
+                padding: 4,
+              }}
+            >
+              {cart.map((item) => (
+                <View
+                  key={item.id}
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginVertical: 10,
+                    padding: 6,
+                    borderRadius: 10,
+                    borderWidth: 1,
+                    borderColor: "lightgrey",
+                  }}
+                >
+                  <View style={{ flexDirection: "row", alignItems: "center" }}>
+                    <Text
+                      style={{
+                        fontSize: 17,
+                        fontWeight: "bold",
+                        marginHorizontal: 10,
+                      }}
+                    >
+                      {item.name}
+                    </Text>
+                  </View>
+                  <View style={{ flexDirection: "row", alignItems: "center" }}>
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        alignItems: "center",
+                        marginHorizontal: 10,
+                        gap: 3,
+                        borderRadius: 5,
+                        borderWidth: 1,
+                        borderColor: "lightgrey",
+                        backgroundColor: "floralwhite",
+                        padding: 5,
+                      }}
+                    >
+                      <Ionicons
+                        onPress={() => {
+                          dispatch(decrementQuantity(item));
+                          dispatch(decrementProductQuantity(item));
+                        }}
+                        name="remove-circle-outline"
+                        size={24}
+                        color="red"
+                      />
+                      <Text style={{ fontSize: 17, fontWeight: "bold" }}>
+                        {item.quantity}
+                      </Text>
+                      <Ionicons
+                        onPress={() => {
+                          dispatch(addToCart(item));
+                          dispatch(incrementProductQuantity(item));
+                        }}
+                        name="add-circle-outline"
+                        size={24}
+                        color="green"
+                      />
+                    </View>
+                    <Text
+                      style={
+                        item.price * item.quantity > 999
+                          ? {
+                              width: 60,
+                              fontSize: 17,
+                              fontWeight: "bold",
+                              color: "#177377",
+                            }
+                          : {
+                              width: 50,
+                              fontSize: 17,
+                              fontWeight: "bold",
+                              color: "#177377",
+                            }
+                      }
+                    >
+                      ${item.price * item.quantity}
+                    </Text>
+                    {/* render remove all items of this type button  */}
+                    <Ionicons
+                      onPress={() => {
+                        dispatch(removeFromCart(item.id));
+                        dispatch(resetProductQuantity(item));
+                      }}
+                      name="trash-outline"
+                      size={24}
+                      color="red"
+                    />
+                  </View>
+                </View>
+              ))}
+            </ScrollView>
+
+            {/* render billing details with list of items  */}
+            <View
+              style={{
+                backgroundColor: "white",
+                marginHorizontal: 10,
+                marginTop: 23,
+                padding: 7,
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 20,
+                  fontWeight: "bold",
+                  textAlign: "center",
+                }}
+              >
+                Billing Details
+              </Text>
+              <View
+                style={{ gap: 5, borderRadius: 7, padding: 10, marginTop: 15 }}
+              >
+                <View style={styles.def}>
+                  <Text style={styles.defText}>Number of items</Text>
+                  <Text style={styles.defText}>{cart.length}</Text>
+                </View>
+                <View style={styles.def}>
+                  <Text style={styles.defText}>Delivery Charges</Text>
+                  <Text
+                    style={styles.defTextBlue}
+                  >
+                    FREE
+                  </Text>
+                </View>
+              </View>
+              <View
+                style={{
+                  padding: 10,
+                  borderTopWidth: 1,
+                  borderColor: "gray",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <Text style={styles.defText}>Date </Text>
+                <Text
+                  style={styles.defTextBlue}
+                >
+                  {route.params.selectedDate.slice(0, 15)}
+                </Text>
+              </View>
+              <View
+                style={{
+                  padding: 10,
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <Text style={styles.defText}>Number of days </Text>
+                <Text
+                  style={styles.defTextBlue}
+                >
+                  {route.params.numberOfDays}
+                </Text>
+              </View>
+              <View
+                style={{
+                  padding: 10,
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <Text style={styles.defText}>Time of pick up</Text>
+                <Text
+                  style={{ fontSize: 17, fontWeight: "300", color: "skyblue" }}
+                >
+                  {route.params.selectedTime}
+                </Text>
+              </View>
+
+              <View
+                style={{
+                  padding: 10,
+                  borderTopWidth: 1,
+                  borderColor: "gray",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <Text
+                  style={{ fontSize: 17, fontWeight: "bold", color: "black" }}
+                >
+                  Total to pay
+                </Text>
+                <Text
+                  style={{ fontSize: 19, fontWeight: "bold", color: "black" }}
+                >
+                  ${total}
+                </Text>
+              </View>
+            </View>
+          </>
+        )}
+      </SafeAreaView>
+      <Pressable style={styles.proceedToPickUpContainer}>
+        <View>
+          <Text style={styles.proceedToPickUpText}>
+            {cart.length} items | ${total}
+          </Text>
+          <Text
+            style={{
+              fontSize: 14,
+              fontWeight: "400",
+              color: "white",
+              marginVertical: 6,
+            }}
+          >
+            extra charges might apply
+          </Text>
+        </View>
+
+        <Text style={{ fontSize: 17, fontWeight: "600" }}>Place Order</Text>
+      </Pressable>
+    </>
+  );
+};
+
+export default CartScreen;
+
+const styles = StyleSheet.create({
+  proceedToPickUpContainer: {
+    backgroundColor: "#088f8f",
+    padding: 10,
+    marginTop: "auto",
+    marginBottom: 30,
+    margin: 15,
+    borderRadius: 7,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  proceedToPickUpText: {
+    fontSize: 17,
+    fontWeight: "600",
+    color: "white",
+  },
+  def: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  defText: { fontSize: 17, fontWeight: "bold", color: "gray" },
+  defTextBlue: { fontSize: 17, fontWeight: "bold", color: "skyblue" },
+});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,129 +1,174 @@
-import { Alert, Pressable, StyleSheet, Text, View, Image, TextInput, ScrollView } from 'react-native'
-import React, { useEffect , useState } from 'react'
-import * as Location from 'expo-location';
-import { EvilIcons } from '@expo/vector-icons';
-import Carousel from '../components/Carousel';
-import Services from '../components/Services';
-import Product from '../components/Product';
-import { services } from '../data/data';
-import { useDispatch, useSelector } from 'react-redux';
-import { setProducts } from '../redux/ProductReducer';
+import {
+  Alert,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import * as Location from "expo-location";
+import { EvilIcons } from "@expo/vector-icons";
+import Carousel from "../components/Carousel";
+import Services from "../components/Services";
+import Product from "../components/Product";
+import { services } from "../data/data";
+import { useDispatch, useSelector } from "react-redux";
+import { setProducts } from "../redux/ProductReducer";
+import { useNavigation } from "@react-navigation/core";
 
 const HomeScreen = () => {
-  const cart = useSelector(state => state.cart.cart); 
+  const cart = useSelector((state) => state.cart.cart);
+  const total = cart.map((item) => item.price * item.quantity).reduce((a, b) => a + b, 0);
   const dispatch = useDispatch();
- 
-    const products = useSelector(state => state.product.product);
-    const [displaycurrentAddress, setDisplaycurrentAddress] = useState('we are loading location');
-    const [locationServiceEnabled, setLocationServiceEnabled] = useState(false);
+  const navigation = useNavigation();
 
+  const products = useSelector((state) => state.product.product);
+  const [displaycurrentAddress, setDisplaycurrentAddress] = useState(
+    "we are loading location"
+  );
+  const [locationServiceEnabled, setLocationServiceEnabled] = useState(false);
 
-    const checkIfLocationEnabled = async () => {
-        let enabled = await Location.hasServicesEnabledAsync();
-        if (!enabled) {
-            Alert.alert('Location service not enabled', 
-            'Pleas enable location', [
-                {
-                  text: 'activate location ',
-                  onPress: () => console.log('Cancel Pressed'),
-                  style: 'cancel',
-                },
-                {text: 'OK', onPress: () => console.log('OK Pressed')},
-              ]);
-          
-        } else { 
-            setLocationServiceEnabled(enabled);
-        };
-    };
-    const getCurrentLocation = async () => {
-        console.log("getCurrentLocation");
-        let { status } = await Location.requestForegroundPermissionsAsync(); 
-        if (status !== 'granted') {
-            Alert.alert('Permission denied', 
-            'allow the app to use location', [
-                {
-                  text: 'Cancel',
-                  onPress: () => console.log('Cancel Pressed'),
-                  style: 'cancel',
-                },
-                {text: 'OK', onPress: () => console.log('OK Pressed')},
-              ]);
+  const checkIfLocationEnabled = async () => {
+    let enabled = await Location.hasServicesEnabledAsync();
+    if (!enabled) {
+      Alert.alert("Location service not enabled", "Pleas enable location", [
+        {
+          text: "activate location ",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel",
+        },
+        { text: "OK", onPress: () => console.log("OK Pressed") },
+      ]);
+    } else {
+      setLocationServiceEnabled(enabled);
+    }
+  };
+  const getCurrentLocation = async () => {
+    console.log("getCurrentLocation");
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      Alert.alert("Permission denied", "allow the app to use location", [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel",
+        },
+        { text: "OK", onPress: () => console.log("OK Pressed") },
+      ]);
+    }
+    let { coords } = await Location.getCurrentPositionAsync();
+    console.log("coords", coords);
 
-        };
-        let { coords } = await Location.getCurrentPositionAsync();
-        console.log("coords",coords);
+    if (coords) {
+      const { latitude, longitude } = coords;
+      let response = await Location.reverseGeocodeAsync({
+        latitude,
+        longitude,
+      });
+      console.log("response", response);
 
-        if (coords) {
-            const { latitude, longitude } = coords;
-            let response = await Location.reverseGeocodeAsync({ latitude, longitude });
-            console.log("response",response);
-            
-            for (let item of response) {
-                let address = `${item.name}, ${item.region}, ${item.postalCode}, ${item.country}`;
-                setDisplaycurrentAddress(address);
-                // break;
-            } 
-        };
-          
-        }
-    useEffect(() => {
-     checkIfLocationEnabled();
-     getCurrentLocation();
-    }, []);
+      for (let item of response) {
+        let address = `${item.name}, ${item.region}, ${item.postalCode}, ${item.country}`;
+        setDisplaycurrentAddress(address);
+        // break;
+      }
+    }
+  };
+  useEffect(() => {
+    checkIfLocationEnabled();
+    getCurrentLocation();
+  }, []);
 
-    useEffect(() => {
-      if (products.length > 0) return;
-      dispatch(setProducts(services));
-
-    }, []);
+  useEffect(() => {
+    if (products.length > 0) return;
+    dispatch(setProducts(services));
+  }, []);
 
   return (
-    <ScrollView style={{marginTop: 50 }}>
+    <>
+    <ScrollView style={{ marginTop: 50 }}>
       {/* location and profile  */}
-        <View style={{flexDirection: "row", alignItems: "center", padding: 10}}>
-            <EvilIcons name="location" size={34} color="#fd5c63" />
-            <View>
-             <Text style={{fontSize: 23, fontWeight: "500"}}>Home</Text>
-             <Text>{displaycurrentAddress}</Text>
-            </View>
+      <View style={{ flexDirection: "row", alignItems: "center", padding: 10 }}>
+        <EvilIcons name="location" size={34} color="#fd5c63" />
+        <View>
+          <Text style={{ fontSize: 23, fontWeight: "500" }}>Home</Text>
+          <Text>{displaycurrentAddress}</Text>
         </View>
-        <Pressable 
-         style={{marginLeft: "auto", marginRight: 7}}
-         onPress={() => console.log("search pressed")}>
-            <Image
-              style={{width: 40, height: 40, borderRadius: 20}}
-              source={require('../assets/user.png')}
-             />
-        </Pressable>
-        {/* search bar */}
-        <View style={{
-          padding: 10, 
-          margin: 10,
-          flexDirection: "row",
-          alignItemsc: "center",
-          justifyContent: "space-between",
-          borderWidth: 0.9 ,
-          borderColor: "#c0c0c0",
-          borderRadius: 7,
-          }}>
-          <TextInput placeholder='search' />
-          <EvilIcons name="search" size={24} color="black" />
-         </View>
-     {/* carousel */}
+      </View>
+      <Pressable
+        style={{ marginLeft: "auto", marginRight: 7 }}
+        onPress={() => console.log("search pressed")}
+      >
+        <Image
+          style={{ width: 40, height: 40, borderRadius: 20 }}
+          source={require("../assets/user.png")}
+        />
+      </Pressable>
+      {/* search bar */}
+      
+      <View style={{
+        padding: 10, 
+        margin: 10,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        borderWidth: 0.9 ,
+        borderColor: "#c0c0c0",
+        borderRadius: 7,
+      }}>
+        <TextInput
+          style={{ flex: 1 }} // Take up 100% of the available space
+          placeholder='search'
+        />
+        <EvilIcons name="search" size={24} color="black" />
+      </View>
+   
+
+      {/* carousel */}
       <Carousel />
       {/* services */}
       <Services />
-       {/* render all product  */}
-       {
-          products.map((product,index) => (
-            <Product key={index} item={product} />
-          ))
-       }
-
+      {/* render all product  */}
+      {products.map((product, index) => (
+        <Product key={index} item={product} />
+      ))}
     </ScrollView>
-  )
-}
+    {
+      total > 0 && (
+        <Pressable 
+        onPress={() => navigation.navigate("PickUp")}
+        style={styles.proceedToPickUpContainer}>
+          <View>
+            <Text style={styles.proceedToPickUpText}>{cart.length} items | ${total}</Text>
+            <Text style={{fontSize: 14, fontWeight: "400", color: "white", marginVertical: 6}}>extra charges might apply</Text>
+          </View>
+            <Text style={{fontSize: 17, fontWeight: "600"}}>Proceed to pick up</Text>
+        </Pressable>
+      ) 
+    }
+    </>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  proceedToPickUpContainer: {
+    backgroundColor: "#088f8f",
+    padding: 10,
+    marginBottom: 30,
+    margin: 15,
+    borderRadius: 7,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  proceedToPickUpText: {
+    fontSize: 17,
+    fontWeight: "600",
+    color: "white",
+  }
+});
