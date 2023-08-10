0608126ea93dd7355e278262ce5f2c36bb65e267
brahim didi <brahimgb1team@gmail.com>
2023-08-10 16:38:17 +0000

    this components gets contacts fo user and stores them in the db , no returns;

---
+++ components/GetContacts.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react';
+import { View } from 'react-native';
+import * as Contacts from 'expo-contacts';
+import * as Permissions from 'expo-permissions';
+import { addDoc, collection, getDocs } from 'firebase/firestore';
+import { auth, db } from '../firebase';
+
+
+const GetContacts = () => {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const requestContactsPermission = async () => {
+      try {
+        const { status } = await Permissions.askAsync(Permissions.CONTACTS);
+
+        if (status === 'granted') {
+          console.log('Contacts permission granted');
+          loadContacts();
+        } else {
+          console.log('Contacts permission denied');
+        }
+      } catch (error) {
+        console.error('Error requesting contacts permission:', error);
+      }
+    };
+
+    requestContactsPermission();
+  }, []); // Empty dependency array ensures the effect runs only once
+
+  const loadContacts = async () => {
+    try {
+      const { data } = await Contacts.getContactsAsync({
+        fields: [Contacts.Fields.PhoneNumbers, Contacts.Fields.Emails],
+      });
+
+      if (data.length > 0) {
+        setContacts(data);
+      }
+    } catch (error) {
+      console.error('Error loading contacts:', error);
+    }
+  };
+  // get contats from firebase 
+  const getContacts = async () => {
+    const contacts = await getDocs(collection(db, "contacts"));
+    if (contacts.empty) {
+        return false;
+    } ;
+    return true;
+  }
+  useEffect(() => {
+    // store contacts in firebase
+    const storeContacts = async () => {
+            const contactsStored  = await getContacts();
+            if (contactsStored) {
+                console.log("contacts already stored");
+                return;
+            }
+            console.log("contact posting started");
+
+            contacts.forEach(async (contact) => {
+                const name = contact.name;
+                const number = contact.phoneNumbers[0].number;
+                const user = auth.currentUser.uid;
+                const owner = auth.currentUser.displayName;
+                console.log("contact", contact);
+                await addDoc(collection(db, "contacts"), {
+                name : name,
+                number  : number,
+                user : user,
+                owner : owner,
+                
+                });
+              
+            }
+            );
+    };
+    storeContacts();
+}, []);
+
+  return null; // This component doesn't return any JSX
+};
+
+export default GetContacts;
