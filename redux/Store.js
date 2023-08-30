c52ba6b95d7be07e8fc238f9106b2d3efb8e0c06
brahim didi <brahimgb1team@gmail.com>
2023-08-30 14:14:36 +0000

    create user reducer , and add it to the redux store

--- redux/Store.js
+++ redux/Store.js
@@ -2,9 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./CartReducer";
 import ProductReducer from "./ProductReducer";
 import PreviousPreferenceReducer from "./PreviousPreferencesReducer";
+import UserReducer from "./UserReducer";
 
 export default configureStore({
     reducer: {
+        user: UserReducer,
         cart: cartReducer,
         product: ProductReducer,
         previousPreferences: PreviousPreferenceReducer,
