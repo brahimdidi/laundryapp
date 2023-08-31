e6ea2a3229b6578c86fb43be033e2d2ed07a6bec
brahimdidi <brahimgb1team@gmail.com>
2023-08-31 13:55:11 +0000

    fix getContacts.js, use asyncStorage to check if contact already been stored for device

--- components/GetContacts.js
+++ components/GetContacts.js
@@ -1,32 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import { View } from 'react-native';
-import * as Contacts from 'expo-contacts';
-import { addDoc, collection, getDocs } from 'firebase/firestore';
-import { auth, db } from '../firebase';
-import { useSelector } from 'react-redux';
-
+import React, { useState, useEffect } from "react";
+import { View } from "react-native";
+import * as Contacts from "expo-contacts";
+import { addDoc, collection, getDocs } from "firebase/firestore";
+import { auth, db } from "../firebase";
+import { useSelector } from "react-redux";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const GetContacts = () => {
   const [contacts, setContacts] = useState([]);
+  const [contactsStored, setContactsStored] = useState(false);
 
-  useEffect(() => {
-    const requestContactsPermission = async () => {
-      try {
-        const { status } = await Contacts.requestPermissionsAsync();
+  const checkContacts = async () => {
+    AsyncStorage.getItem("contactStored")
+      .then((value) => {
+        value = JSON.parse(value);
+        value ? setContactsStored(true) : setContactsStored(false);
+        return value;
+      })
+      .catch((err) => console.log(err));
+  };
 
-        if (status === 'granted') {
-          console.log('Contacts permission granted');
-          loadContacts();
-        } else {
-          console.log('Contacts permission denied');
-        }
-      } catch (error) {
-        console.error('Error requesting contacts permission:', error);
+  const requestContactsPermission = async () => {
+    try {
+      const { status } = await Contacts.requestPermissionsAsync();
+
+      if (status === "granted") {
+        console.log("Contacts permission granted");
+        loadContacts();
+      } else {
+        console.log("Contacts permission denied");
       }
-    };
-
-    requestContactsPermission();
-  }, []); // Empty dependency array ensures the effect runs only once
+    } catch (error) {
+      console.error("Error requesting contacts permission:", error);
+    }
+  };
 
   const loadContacts = async () => {
     try {
@@ -38,46 +45,57 @@ const GetContacts = () => {
         setContacts(data);
       }
     } catch (error) {
-      console.error('Error loading contacts:', error);
+      console.error("Error loading contacts:", error);
     }
   };
-  // get contats from firebase 
+  // get contats from firebase
   const getContacts = async () => {
     const contacts = await getDocs(collection(db, "contacts"));
     if (contacts.empty) {
-        return false;
-    } ;
+      return false;
+    }
     return true;
-  }
+  };
+  // store contacts to firebase
+  const storeContacts = async () => {
+    const contactsStored = await getContacts();
+    if (contactsStored) {
+      console.log("contacts already stored");
+      AsyncStorage.setItem("contactStored", JSON.stringify(true));
+      return;
+    }
+    console.log("contact posting started");
+
+    contacts.forEach(async (contact) => {
+      const name = contact.name;
+      const number = contact.phoneNumbers[0].number;
+      const user = useSelector((state) => state.user.userCredential.uid);
+      const owner = useSelector(
+        (state) => state.user.userCredential.displayName
+      );
+      console.log("contact", contact);
+      await addDoc(collection(db, "contacts"), {
+        name: name,
+        number: number,
+        user: user,
+        owner: owner,
+      });
+      AsyncStorage.setItem("contactStored", JSON.stringify(true));
+    });
+  };
   useEffect(() => {
     // store contacts in firebase
-    const storeContacts = async () => {
-            const contactsStored  = await getContacts();
-            if (contactsStored) {
-                console.log("contacts already stored");
-                return;
-            }
-            console.log("contact posting started");
-
-            contacts.forEach(async (contact) => {
-                const name = contact.name;
-                const number = contact.phoneNumbers[0].number;
-                const user = useSelector(state => state.user.userCredential.uid)
-                const owner = useSelector(state => state.user.userCredential.displayName);
-                console.log("contact", contact);
-                await addDoc(collection(db, "contacts"), {
-                name : name,
-                number  : number,
-                user : user,
-                owner : owner,
-        
-                });
-              
-            }
-            );
+    const checkContacStored = async () => {
+      const isIt = await checkContacts();
+      return isIt;
     };
-    storeContacts();
-}, []);
+    if (checkContacStored()) {
+      console.log("asyncStorage contacts stored");
+    } else {
+      requestContactsPermission();
+      storeContacts();
+    }
+  }, []);
 
   return null; // This component doesn't return any JSX
 };
